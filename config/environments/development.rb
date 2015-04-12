2e25fd60e4823093ef2c42e2c145c36b319af086
Dezruk <dezrukplays@gmail.com>
2015-04-11 21:34:16 -0400

    Implemented Devise for Users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,10 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  #Required for Heroku
+  #Note to set LocalHost to regular domain name
+  config.action_mailer.default_url_options = { host: 'LocalHost', port: 3000 }
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Required for Heroku
+  #Note to set LocalHost to regular domain name
+  config.action_mailer.default_url_options = { host: 'LocalHost', port: 3000 }
 end
